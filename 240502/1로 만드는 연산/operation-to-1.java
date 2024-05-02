1fd14cfa43b499393a4b366e74c74f886f02d7fd
김영록 <31751611+shim99887@users.noreply.github.com>
2024-05-02 13:19:58 +0900

    Title: 1로 만드는 연산, Time: 3068ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240502/1로 만드는 연산/operation-to-1.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.math.BigDecimal;
+
+public class Main {
+    static String testcase = "9";
+
+    static int n;
+    static int[] memo;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new StringReader(testcase));
+
+        n = Integer.parseInt(br.readLine());
+
+        memo = new int[n + 2];
+
+        solve(n, 0);
+
+        System.out.println(memo[1]);
+    }
+
+    static void solve(int number, int cnt){
+        if(number <= 0){
+            return;
+        }
+        if(number == 1){
+            if(memo[number] == 0){
+                memo[number] = cnt;
+            } else {
+                memo[number] = Math.min(memo[number], cnt);
+            }
+            return;
+        }
+
+        if(memo[number] > 0 && memo[number] < cnt){
+            return;
+        }
+
+        if(number >= 3 && number % 3 == 0){
+            solve(number / 3, cnt + 1);
+        }
+
+        if(number >= 2 && number % 2 == 0){
+            solve(number / 2, cnt + 1);
+        }
+
+        solve(number - 1, cnt + 1);
+    }
+}
