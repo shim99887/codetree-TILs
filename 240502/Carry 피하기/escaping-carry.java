e707d85dc9648c5128f4fe4997730f84a413de15
김영록 <31751611+shim99887@users.noreply.github.com>
2024-05-02 12:43:54 +0900

    Title: Carry 피하기, Time: 141ms, Memory: 9MB, Status: Passed - CodeTree

--- 240502/Carry 피하기/escaping-carry.java
+++ 240502/Carry 피하기/escaping-carry.java
@@ -3,27 +3,22 @@ import java.util.*;
 
 public class Main {
 
-    static String testcase = "20\n" +
-            "54131012\n" +
-            "222411\n" +
-            "11000310\n" +
-            "1530111\n" +
-            "15001011\n" +
-            "21234311\n" +
-            "12211121\n" +
-            "101211\n" +
-            "21114541\n" +
-            "24115401\n" +
-            "122001\n" +
-            "2012114\n" +
-            "12011412\n" +
-            "21120235\n" +
-            "2141525\n" +
-            "5121521\n" +
-            "32252150\n" +
-            "23321012\n" +
-            "12500125\n" +
-            "12513510";
+    static String testcase = "15\n" +
+            "241032\n" +
+            "6530050\n" +
+            "12210124\n" +
+            "46100501\n" +
+            "533231\n" +
+            "28290465\n" +
+            "1101021\n" +
+            "17245104\n" +
+            "3745209\n" +
+            "10001\n" +
+            "10034301\n" +
+            "43216500\n" +
+            "19984654\n" +
+            "53552499\n" +
+            "20003100";
 
 
     static int n, answer = 0;
@@ -37,15 +32,12 @@ public class Main {
 
         arr = new int[n];
         memo = new int[n];
-//        visited = new boolean[n];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-//        for (int i = 0; i < n; i++) {
-            solve(0, 0, 0);
-//        }
+        solve(0, 0, 0);
 
         System.out.println(answer);
     }
@@ -57,8 +49,8 @@ public class Main {
         }
 
         for (int i = depth; i < n; i++) {
-            if(check(sum, arr[i])){
-                solve(depth + 1, cnt + 1, sum + arr[i]);
+            if (check(sum, arr[i])) {
+                solve(i + 1, cnt + 1, sum + arr[i]);
             }
         }
 
@@ -66,8 +58,8 @@ public class Main {
 
     static boolean check(int sum, int target) {
 
-        while(sum > 0 || target > 0){
-            if((sum % 10 + target % 10) >= 10){
+        while (sum > 0 || target > 0) {
+            if ((sum % 10 + target % 10) >= 10) {
                 return false;
             }
             sum /= 10;
