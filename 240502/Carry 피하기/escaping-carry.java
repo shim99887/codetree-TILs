0c09f033346ae711ee662fdece5fb6b0bacef836
김영록 <31751611+shim99887@users.noreply.github.com>
2024-05-02 12:40:21 +0900

    Title: Carry 피하기, Time: 114ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240502/Carry 피하기/escaping-carry.java
+++ 240502/Carry 피하기/escaping-carry.java
@@ -3,12 +3,27 @@ import java.util.*;
 
 public class Main {
 
-    static String testcase = "5\n" +
-            "522\n" +
-            "6\n" +
-            "84\n" +
-            "7311\n" +
-            "19\n";
+    static String testcase = "20\n" +
+            "54131012\n" +
+            "222411\n" +
+            "11000310\n" +
+            "1530111\n" +
+            "15001011\n" +
+            "21234311\n" +
+            "12211121\n" +
+            "101211\n" +
+            "21114541\n" +
+            "24115401\n" +
+            "122001\n" +
+            "2012114\n" +
+            "12011412\n" +
+            "21120235\n" +
+            "2141525\n" +
+            "5121521\n" +
+            "32252150\n" +
+            "23321012\n" +
+            "12500125\n" +
+            "12513510";
 
 
     static int n, answer = 0;
@@ -28,58 +43,35 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        for (int i = 0; i < n; i++) {
-            solve(i, new boolean[n]);
-        }
+//        for (int i = 0; i < n; i++) {
+            solve(0, 0, 0);
+//        }
 
         System.out.println(answer);
     }
 
-    static void solve(int depth, boolean[] visited) {
+    static void solve(int depth, int cnt, int sum) {
+        answer = Math.max(answer, cnt);
         if (depth >= n) {
             return;
         }
 
         for (int i = depth; i < n; i++) {
-            if (!visited[i] && check(arr[i], visited)) {
-                visited[i] = true;
-                solve(i + 1, visited);
-                visited[i] = false;
+            if(check(sum, arr[i])){
+                solve(depth + 1, cnt + 1, sum + arr[i]);
             }
         }
 
-        int temp = 0;
-
-        for(int i=0; i < visited.length; i++){
-            if(visited[i]){
-                temp++;
-            }
-        }
-        answer = Math.max(answer, temp);
     }
 
-    static boolean check(int value, boolean[] visited) {
-        List<Integer> arr2 = new ArrayList<>();
-        arr2.add(value);
-        int maxLen = String.valueOf(value).length();
+    static boolean check(int sum, int target) {
 
-        for (int i = 0; i < visited.length; i++) {
-            if (visited[i]) {
-                arr2.add(arr[i]);
-                maxLen = Math.max(maxLen, String.valueOf(arr[i]).length());
-            }
-        }
-
-        for (int i = 0; i < maxLen; i++) {
-            int temp = 0;
-            for (int j = 0; j < arr2.size(); j++) {
-                temp += arr2.get(j) % 10;
-                arr2.set(j, arr2.get(j) / 10);
-            }
-
-            if (temp > 10) {
+        while(sum > 0 || target > 0){
+            if((sum % 10 + target % 10) >= 10){
                 return false;
             }
+            sum /= 10;
+            target /= 10;
         }
         return true;
     }
